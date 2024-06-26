69de10dba52d7594e4d33cc43e5fdf3a87ea64d2
Brassat Alexandru <alrx.brss@gmail.com>
2024-06-26 18:50:53 +0300

    fin

--- impl2/proj/mapper.py
+++ impl2/proj/mapper.py
@@ -6,8 +6,10 @@ import matplotlib, tqdm
 from mpl_toolkits.mplot3d import Axes3D
 import psycopg2.pool as psycopg2_pool
 from utils.dset import *
+import utils.preprocess as pp
 import torch, numpy as np, dill, cloudpickle, sys, glob
 from torchmetrics.regression import R2Score
+import matplotlib.ticker as mticker
 file = r'ClimSim_low-res_grid-info.nc'
 grid = xr.open_dataset(file,engine='netcdf4')
 
@@ -34,15 +36,19 @@ def load_model(path):
 		model = d['model']
 	return model
 
-def map_plot(errs):
+def map_plot(errs, levels):
 	matplotlib.rcParams.update({'font.size':15})
 	fig, ax = plt.subplots() #layout='constrained')
 	fig.set_size_inches(6.5, 2.5)
 	m = Basemap(ax=ax, projection='robin',lon_0=0,resolution='c')
 	x,y = m(grid.lon,grid.lat)
 	m.drawcoastlines(linewidth=0.5)
-	plot = plt.tricontourf(x,y,errs,cmap='viridis',levels=30)
-	m.colorbar(plot, ax=ax, fig=fig)
+	errs = np.clip(errs, -250, 1)
+	plot = plt.tricontourf(x,y,errs,cmap='viridis',levels=50, vmin=-250, vmax=1)
+
+	fig.colorbar(plot, ax=ax,
+				ticks=[-i for i in reversed(range(0, 260, 50))], extend='min')
+	return plot.levels
 
 
 def get_errs(model, dset):
@@ -50,40 +56,51 @@ def get_errs(model, dset):
 	model.to(DEVICE)
 	with torch.no_grad():
 		losses = []
-		r2score = R2Score(num_outputs=368, multioutput='raw_values').to(DEVICE)
+		r2score = R2Score(num_outputs=368, multioutput='raw_values') #.to(DEVICE)
 		ldata = int(dset[0].shape[0]//384)
-		print(ldata)
+		# print(ldata)
 		for i in range(384):
 			din, dout = dset
 			din, dout = din[[i + j*384 for j in range(ldata)], :], dout[[i + j*384 for j in range(ldata)], :]
 			din, dout = torch.tensor(din, dtype=torch.float64).to(DEVICE), torch.tensor(dout, dtype=torch.float64).to(DEVICE)
 			prediction = model(din)
-			losses.append(r2score(prediction, dout).cpu().numpy().mean().item())
+			v_out = pp.preprocess_functions['standardisation']['norm'](pp.preprocess_functions[PREPROC]['denorm'](dout.cpu().numpy()))
+			pred = pp.preprocess_functions['standardisation']['norm'](pp.preprocess_functions[PREPROC]['denorm'](prediction.cpu().numpy()))
+			losses.append(r2score(torch.tensor(pred), torch.tensor(v_out)).cpu().numpy().mean().item())
 	return losses
 
+ldata = 60 #60
+PREPROC = 'none'
+dset = get_splits(PREPROC, dset_class=TimestepSQLDataset, fraction=0.05)[0].__getitems__(range(ldata))
 
 # pbar = tqdm.tqdm(glob.glob('../models/*'))
+# lvls = None
 # for path in pbar:
-# 	try:
+# 	# try:
+# 		if 'gmm' in path:
+# 			continue
 # 		name = path.split('\\')[-1].split('.')[0]
+# 		if name not in ['model-resnet6-----']:
+# 			continue
 # 		pbar.set_postfix_str(f"{name}")
 
 # 		model = load_model(path)
 # 		errs = get_errs(model, dset)
 
-# 		map_plot(errs)
+# 		lvls = map_plot(errs, lvls)
 # 		plt.title(name)
-# 		plt.savefig(f'figures/{name}.png')
-# 		# plt.show()
-# 	except:
-# 		print('error', path)
-
-dset = get_splits('none', dset_class=TimestepSQLDataset, fraction=0.05)[0].__getitems__(range(60))
-# print(dset[0].shape)
+# 		plt.show()
+# 		# exit(0)
+# 		# plt.savefig(f'figures2/{name}.png')
+# 		plt.close()
+# 	# except:
+# 	# 	print('error', path)
+# # print(dset[0].shape)
+# exit(0)
 torch.set_default_dtype(torch.float64)
 import utils.preprocess as pp
 r2score = R2Score(num_outputs=368).to('cpu')
-r2s = np.array(368)
+r2s = np.zeros(384)
 
 model = cloudpickle.load(open("../impl/gmm_model.pickle", 'rb'))
 for i in range(384):
@@ -101,8 +118,8 @@ for i in range(384):
 	# preds.append(pred)
 	r2 = r2score(pred, v_out)
 	r2tot += r2.cpu().numpy()
-	r2s[i] = r2s / 6
-map_plot(r2s)
+	r2s[i] = r2tot / 6
+map_plot(r2s, None)
 plt.title('GMM')
 plt.imsave('figures/gmm.png')
 plt.show()
